ed98b5f8080c5714bcac2b235111998a0d4c99ca
m-amoura <m-amoura@enageo.com>
2024-10-04 11:06:55 +0100

    first commit

---
+++ src/main/java/com/amoura/bookapi/BookApiGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.amoura.bookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApiGraphQlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookApiGraphQlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/amoura/bookapi/BookApiGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amoura.bookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApiGraphQlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
